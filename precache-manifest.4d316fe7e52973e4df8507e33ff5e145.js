cff330242dbcf3e386410c8175b78b86115620f8
Siddhant Mishra <siddhant48311@gmail.com>
2020-05-30 05:09:58 +0530

    Updates

---
+++ precache-manifest.4d316fe7e52973e4df8507e33ff5e145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa39d109f501c73decb7900c4aa79329",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45f85ba614834b78d544",
+    "url": "/monsters-rolodex/static/css/main.274b10c4.chunk.css"
+  },
+  {
+    "revision": "ef6aa4c9c0b65164e432",
+    "url": "/monsters-rolodex/static/js/2.e84d26c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e84d26c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f85ba614834b78d544",
+    "url": "/monsters-rolodex/static/js/main.ff759dbd.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
